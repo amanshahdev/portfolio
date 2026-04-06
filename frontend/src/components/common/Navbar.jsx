5a4c5a85310cc7df12692218e7b8008bf6caeb29
Aman Shah <amnshh216@gmail.com>
2026-04-06 14:58:34 +0545

    minor changes made

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -89,7 +89,7 @@ export default function Navbar() {
       {/* Mobile Menu - Simple Conditional Rendering */}
       {mobileOpen && (
         <div className="fixed top-[60px] left-0 right-0 z-[99] bg-black/95 border-b border-white/10 md:hidden shadow-lg">
-          <nav className="max-w-7xl mx-auto px-4 md:px-6 py-4 flex flex-col gap-2">
+          <nav className="w-full max-w-7xl mx-auto px-4 md:px-6 py-4 flex flex-col gap-2">
             {navLinks.map((link) => (
               <NavLink
                 key={link.path}
